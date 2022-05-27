e6f0b8f5f9f933336ee2642adf2de55edd0af3b9
Ubiratan <r14.ubiratan.junior@gmail.com>
2022-05-27 17:58:45 -0300

    adicionando script de selamento das secrets

---
+++ scripts/clone-sc-cm/seal_secrets.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+function main(){
+    NAMESPACES_PATH="$1"
+    for dir in $NAMESPACES_PATH/*/ ; do
+        if [ -d "$dir/secrets" ]; then
+            pushd "$dir/secrets"
+            for secret in *; do
+                secret_name=$(echo $secret | awk -F  "." '{print $1}')
+                echo -e " /kubeseal<$secret > ${TEMPLATES_PATH}/$secret_name/${secret_name}_sealed.yaml -o yaml"
+            done
+            popd
+        else
+            echo "No secret found in the namespace"
+        fi
+    done
+}
+
+
+TEMPLATES_PATH="$2"
+main "$1"
